f4e189760da56714d4056186eb211d9139fb5848
tim.chen73 <tim.chen@titansoft.com.sg>
2022-11-03 00:22:37 +0800

    dilly helper

---
+++ MinerMVC/Controllers/DillyController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MinerMVC.Controllers;
+
+public class DillyController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
