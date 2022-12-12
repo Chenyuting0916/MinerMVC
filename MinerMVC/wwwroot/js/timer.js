7517e4f758ac9c220b4ea4106583655468f13b6b
tim.chen73 <tim.chen@titansoft.com.sg>
2022-12-13 01:40:21 +0800

    add countdown timer 30mins

--- MinerMVC/wwwroot/js/timer.js
+++ MinerMVC/wwwroot/js/timer.js
@@ -1,1 +1,31 @@
-﻿
+﻿const oneSecond = 1000;
+const oneMinutes = oneSecond * 60;
+const oneHour = oneMinutes * 60;
+
+
+$(document).ready(() => {
+    $("#start").click(() => {
+        startCountDown();
+        $("#start").remove();
+    });
+});
+
+
+function startCountDown() {
+    let countDownTime = 30 * 60 * 1000;
+
+    let x = setInterval(function () {
+
+        let minutes = Math.floor((countDownTime % oneHour) / oneMinutes);
+        let seconds = Math.floor((countDownTime % oneMinutes) / oneSecond);
+
+        document.getElementById("demo").innerHTML = minutes + "m " + seconds + "s ";
+
+        if (countDownTime <= 0) {
+            clearInterval(x);
+        }
+
+        countDownTime -= oneSecond;
+    }, oneSecond);
+}
+
