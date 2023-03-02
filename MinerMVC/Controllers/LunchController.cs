9c67e911c3a711bfbae7200372fcd79c78772746
tim.chen73 <tim.chen@titansoft.com.sg>
2023-03-02 22:37:19 +0800

    test api

---
+++ MinerMVC/Controllers/LunchController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MinerMVC.Models.CustomExcelDb;
+using MinerMVC.Services;
+
+namespace MinerMVC.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class LunchController : ControllerBase
+{
+    private readonly ICustomExcelService _customExcelService;
+
+    public LunchController(ICustomExcelService customExcelService)
+    {
+        _customExcelService = customExcelService;
+    }
+
+    [HttpGet]
+    [Route("Test")]
+    public List<CustomExcel> Test()
+    {
+        return _customExcelService.GetAll().ToList();
+    }
+}
