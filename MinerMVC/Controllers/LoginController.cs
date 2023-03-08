eafe689bf0474c04f3e259ce32e3f47e0d627872
昱廷 陳 <chenyuting0916@hotmail.com>
2023-03-08 22:28:18 +0800

    add login page frontend

---
+++ MinerMVC/Controllers/LoginController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MinerMVC.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult CreateAccount()
+        {
+            return View();
+        }
+
+        public IActionResult ForgetPassword()
+        {
+            return View();
+        }
+    }
+}
